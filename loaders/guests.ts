edcedd12745600059657efd2f5daf093bed3f60e
decobot <capy@deco.cx>
2024-09-12 17:58:14 -0300

    google spreadsheet added

---
+++ loaders/guests.ts
@@ -0,0 +1,51 @@
+import creds from "site/static/lecaegui-ebd973469e8e.json" with { type: "json" };
+import { JWT } from 'google-auth-library';
+import { GoogleSpreadsheet } from 'google-spreadsheet'; 
+
+export interface Guest {
+    Nome: string;
+    Email: string;
+    Confirmed: string;
+    ConfirmationDate: string;
+    TrazMaisUm: string;
+    NomeMaisUm: string;
+    Papel: string;
+    Origem: string;
+    LecaGui: string;
+    EMaisUm: string;
+    PodemosCortar: string;
+}
+
+export interface GuestList {
+    data : Guest[]
+}
+
+const SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+];
+
+const jwt = new JWT({
+    email: creds.client_email,
+    key: creds.private_key,
+    scopes: SCOPES,
+});
+
+const doc = new GoogleSpreadsheet('1_nIZM0ZGiw_zoEE6CLBamVZevr6NMTZkronP7mj1O7I', jwt);
+
+export default async function guests(): Promise<GuestList> {
+
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[0]
+    const rows = await sheet.getRows()
+
+    const filteredNames = rows
+    .filter(row => !row.get('É +1?'))
+    .map(row => row.get('Nome'));
+
+    console.log(filteredNames)
+
+    const guests = filteredNames
+
+    return {data: guests};
+}
