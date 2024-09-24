e855d9f5c2064834d99bf66131d71937dd681628
decobot <capy@deco.cx>
2024-09-24 17:31:01 -0300

    fix: header style

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -11,10 +11,9 @@ export interface Nav {
 
 export default function Header({text, image, alt, width, height} : Nav) {
   return (
-    <nav class="py-8 lg:px-0 px-6 bottom-0 w-full mx-auto">
-      <div class="flex flex-row items-center justify-between text-xl">
-        <div class="w-1/3"></div>
-        <div class="flex items-center justify-center w-1/3">
+    <nav className="bg-[#ECE1D3] py-8 px-6 w-full">
+      <div className="max-w-7xl mx-auto flex flex-row items-center justify-between text-xl">
+        <div className="flex items-center">
           {text}
           {image && (
             <Image
@@ -22,11 +21,11 @@ export default function Header({text, image, alt, width, height} : Nav) {
               alt={alt || ""}
               height={height || 20}
               width={width || 50}
-              class="ml-2"
+              className="ml-2"
             />
           )}
         </div>
-        <div class="flex gap-4 w-1/3 justify-end pr-6">
+        <div className="flex gap-4">
           <a href="#herosided" onClick={(e) => handleSmoothScroll(e, 'herosided')}>Rsvp</a>
           <a href="#photogrid" onClick={(e) => handleSmoothScroll(e, 'photogrid')}>Data</a>
           <a href="#faq" onClick={(e) => handleSmoothScroll(e, 'faq')}>Sobre</a>
