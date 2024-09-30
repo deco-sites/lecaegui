38278053ca0f85ee582f63851545c6836afccaff
decobot <capy@deco.cx>
2024-09-30 17:28:46 -0300

    feat: abstracting google credentials

--- actions/confirmGuest.ts
+++ actions/confirmGuest.ts
@@ -1,6 +1,4 @@
-import creds from "../static/lecaegui-ebd973469e8e.json" with { type: "json" }
-import { JWT } from 'google-auth-library';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { doc } from "site/static/googleCreds.ts";
 
 export interface Props {
     name: string
@@ -9,19 +7,6 @@ export interface Props {
 export interface Result {
     status: "ok" | "fail"
 }
-
-const SCOPES = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive.file',
-];
-
-const jwt = new JWT({
-    email: creds.client_email,
-    key: creds.private_key,
-    scopes: SCOPES,
-});
-
-const doc = new GoogleSpreadsheet('1_nIZM0ZGiw_zoEE6CLBamVZevr6NMTZkronP7mj1O7I', jwt);
 
 export default async function confirmGuest(props: Props): Promise<Result> {
     try {
