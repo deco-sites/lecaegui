427bbe38d3e97678593b4867bcd4e8add7a0f53f
decobot <capy@deco.cx>
2024-10-03 14:59:43 -0300

    fix add guests

--- actions/addguests.ts
+++ actions/addguests.ts
@@ -1,38 +1,37 @@
 import { doc } from "site/static/googleCreds.ts";
 
 export interface Props {
-    name: string
-    origin: string
+  name: string;
+  origin: string;
 }
 
 export interface Result {
-    status: "ok" | "fail";
+  status: "ok" | "fail";
 }
 
-export default async function addGuest(props : Props) : Promise<Result> {
+export default async function addGuest(props: Props): Promise<Result> {
+  try {
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    const rows = await sheet.getRows();
 
-    try {
-        await doc.loadInfo();
-        const sheet = doc.sheetsByIndex[0];
-        const rows = await sheet.getRows();
+    const { name, origin } = props;
 
-        const { name, origin } = props
+    const guestRow = rows.find((row) => row.get("Nome") === origin);
 
-        const guestRow = rows.find((row) => row.get("Nome") === origin);
+    if (guestRow) {
+      guestRow.set("Traz +1", "Sim");
+      guestRow.save();
 
-        if(guestRow) {
-            guestRow.set("Traz +1", "Sim")
-            guestRow.save()
-
-            await sheet.addRow({
-                "Nome": name,
-                "Origem": origin,
-                "É +1": "Sim"
-            })
-        }
-        return {status: "ok"}
-    } catch (error) {
-        console.error("Error confirming guest:", error);
-        return { status: "fail" };
-      }
+      await sheet.addRow({
+        "Nome": name,
+        "Origem": origin,
+        "É +1": "Sim",
+      });
+    }
+    return { status: "ok" };
+  } catch (error) {
+    console.error("Error confirming guest:", error);
+    return { status: "fail" };
+  }
 }
